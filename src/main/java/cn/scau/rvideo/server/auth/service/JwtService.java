412fc4a6a9b460298d8af8fa8b70d3d4fc3ab6ac
cbc <827718623@qq.com>
2018-11-26 01:53:36 +0800

    refactor(auth): merge jwtService and jwtUtils

--- src/main/java/cn/scau/rvideo/server/auth/service/JwtService.java
+++ src/main/java/cn/scau/rvideo/server/auth/service/JwtService.java
@@ -2,15 +2,14 @@ package cn.scau.rvideo.server.auth.service;
 
 import cn.scau.rvideo.server.auth.AuthProperties;
 import cn.scau.rvideo.server.auth.token.UserToken;
-import cn.scau.rvideo.server.auth.util.JwtUtils;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import javax.crypto.SecretKey;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
+import java.util.List;
 
 @Service
 public class JwtService {
@@ -18,18 +17,37 @@ public class JwtService {
     private AuthProperties authProperties;
     private static final SecretKey key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
 
-    public void addToken(UserToken userToken, HttpServletResponse res) {
-        final String JWT = JwtUtils.createJWT(userToken, authProperties.getToken().getExpiration().toMillis(), key);
-        // 跨域暴露响应头字段
-        res.setHeader("Access-Control-Expose-Headers", authProperties.getHeaderName());
-        res.setHeader(authProperties.getHeaderName(), authProperties.getTokenPrefix() + JWT);
+    public String createToken(UserToken userToken) {
+        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+        long nowMillis = System.currentTimeMillis();
+        Date now = new Date(nowMillis);
+        // 设置JWT
+        JwtBuilder builder = Jwts.builder().setId(userToken.getId().toString())
+                .setIssuedAt(now)
+                .claim("name", userToken.getName())
+                .claim("email", userToken.getEmail())
+                .claim("roles", userToken.getRoles())
+                .signWith(key,signatureAlgorithm);
+        // 设置过期时间
+        long ttlMillis = authProperties.getToken().getExpiration().toMillis();
+        if (ttlMillis >= 0) {
+            long expMillis = nowMillis + ttlMillis;
+            Date exp = new Date(expMillis);
+            builder.setExpiration(exp);
+        }
+        return authProperties.getTokenPrefix() + builder.compact();
     }
 
-    public UserToken verifyToken(HttpServletRequest request) throws JwtException {
-        final String token = request.getHeader(authProperties.getHeaderName());
-        if (token != null && !"null".equals(token)){
-            return JwtUtils.parseJWT(token.replace(authProperties.getTokenPrefix(), ""), key);
-        }
+    public UserToken parseToken(String token) throws JwtException {
+        token = token.replace(authProperties.getTokenPrefix(), "");
+        Claims claims = Jwts.parser()
+                .setSigningKey(key)
+                .parseClaimsJws(token).getBody();
+        UserToken tokenUser = new UserToken();
+        tokenUser.setId(Integer.parseInt(claims.getId()));
+        tokenUser.setName(claims.get("name", String.class));
+        tokenUser.setEmail(claims.get("email", String.class));
+        tokenUser.setRoles((List<String>) claims.get("roles", List.class));
         return null;
     }
 }
