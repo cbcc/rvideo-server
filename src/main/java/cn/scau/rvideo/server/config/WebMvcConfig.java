1e79dd179f951d41ac725dc1e81d0f03d8677bc9
cbc <827718623@qq.com>
2018-11-29 21:42:21 +0800

    feat(config): update resource and file config

--- src/main/java/cn/scau/rvideo/server/config/WebMvcConfig.java
+++ src/main/java/cn/scau/rvideo/server/config/WebMvcConfig.java
@@ -2,11 +2,15 @@ package cn.scau.rvideo.server.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.unit.DataSize;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.annotation.Resource;
+import javax.servlet.MultipartConfigElement;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
@@ -18,6 +22,16 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         logger.info("资源路径: " + resourceProperties.getPath());
-        registry.addResourceHandler("/rvideo/resource/**").addResourceLocations(resourceProperties.getPath());
+        registry.addResourceHandler("/rvideo/resource/**").addResourceLocations("file:" + resourceProperties.getPath() + "/");
+    }
+
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        // 单个文件最大2G
+        factory.setMaxFileSize(DataSize.ofGigabytes(2));
+        // 设置总上传数据总大小3G
+        factory.setMaxRequestSize(DataSize.ofGigabytes(3));
+        return factory.createMultipartConfig();
     }
 }
